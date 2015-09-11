54675de9fa8f7d0f19f34a766aa68c2a6c9a1c89
fengyouchao <fengyouchao@gmail.com>
2015-09-11 17:02:53 +0800

    Test JdbcBasedUserManager by H2 database

--- src/test/java/fucksocks/test/server/manager/TestJdbcBasedUserManager.java
+++ src/test/java/fucksocks/test/server/manager/TestJdbcBasedUserManager.java
@@ -28,38 +28,43 @@ import org.junit.Test;
 
 /**
  * Test {@link JdbcBasedUserManager}
- * <br>Created by fengyouchao on 9/9/15.
+ *
+ * @author Youchao Feng
+ * @version 1.0
+ * @date Sep 09, 2015
  */
 public class TestJdbcBasedUserManager {
 
+  private static final String DEFAULT_USERNAME = "fucksocks";
+  private static final String DEFAULT_PASSWORD = "123";
+  private static final String JDBC_CONFIG_FILE = "classpath:jdbc.properties";
+  private static final String SQL_DROP_TABLE = "DROP TABLE `SOCKS_USERS` IF EXISTS";
+  private static final String SQL_CREATE_TABLE =
+      "CREATE TABLE SOCKS_USERS (`username` varchar(50) PRIMARY KEY, `password` varchar(32))";
   private JdbcTemplate jdbcTemplate;
   private JdbcBasedUserManager userManager;
-  private static final String USERNAME = "fucksocks";
-  private static final String PASSWORD = "123";
-  private static final String JDBC_CONFIG_FILE = "classpath:jdbc.properties";
 
-  @Before
-  public void before(){
+  @Before public void before() {
     JdbcConfiguration configuration = JdbcConfiguration.load(JDBC_CONFIG_FILE);
     BasicDataSource dataSource = new BasicDataSource();
+    assert configuration != null;
     dataSource.setUrl(configuration.getUrl());
     dataSource.setUsername(configuration.getUsername());
     dataSource.setPassword(configuration.getPassword());
     userManager = new JdbcBasedUserManager(dataSource);
     jdbcTemplate = userManager.getJdbcTemplate();
+    jdbcTemplate.update(SQL_DROP_TABLE);
+    jdbcTemplate.update(SQL_CREATE_TABLE);
+  }
+
+  @After public void after() {
     jdbcTemplate.deleteAll(JdbcBasedUserManager.USER_TABLE_NAME);
   }
 
-  @After
-  public void after(){
-    jdbcTemplate.deleteAll(JdbcBasedUserManager.USER_TABLE_NAME);
-  }
-
-  @Test
-  public void createAndFind(){
-    User user = new User(USERNAME, PASSWORD);
+  @Test public void createAndFind() {
+    User user = new User(DEFAULT_USERNAME, DEFAULT_PASSWORD);
     userManager.create(user);
-    final User result = userManager.find(USERNAME);
+    final User result = userManager.find(DEFAULT_USERNAME);
 
     Assert.assertEquals(user.getUsername(), result.getUsername());
     Assert.assertEquals(user.getPassword(), result.getPassword());
