2f158b368ea5dd9d41cea36c05497b6132510578
fengyouchao <fengyouchao@gmail.com>
2015-10-09 21:51:19 +0800

    Support server proxy and SSL

--- src/main/java/fucksocks/quickstart/Socks5Server.java
+++ src/main/java/fucksocks/quickstart/Socks5Server.java
@@ -1,6 +1,10 @@
 package fucksocks.quickstart;
 
+import fucksocks.client.SSLSocks5;
+import fucksocks.client.Socks5;
+import fucksocks.client.SocksProxy;
 import fucksocks.common.SSLConfiguration;
+import fucksocks.common.UsernamePasswordCredentials;
 import fucksocks.common.methods.NoAuthenticationRequiredMethod;
 import fucksocks.common.methods.UsernamePasswordMethod;
 import fucksocks.server.SocksProxyServer;
@@ -15,6 +19,7 @@ import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
 import java.io.IOException;
+import java.net.InetSocketAddress;
 
 /**
  * The class <code>Socks5Server</code> can create a simple Socks5 server.
@@ -50,7 +55,10 @@ public class Socks5Server {
     ArgUtil argUtil = new ArgUtil(args);
     int port = 1080;
     String authValue = null;
-    String sslConfigFilePath = null;
+    String sslValue = null;
+    String proxySslValue = null;
+    SocksProxy proxy = null;
+    String proxyValue = null;
 
     if (argUtil.hasArgsIn("-h", "--help")) {
       showHelp();
@@ -61,9 +69,12 @@ public class Socks5Server {
     port = argUtil.getIntValueFromArg("--port=", "=", port);
     authValue = argUtil.getValue("-a", null);
     authValue = argUtil.getValueFromArg("--auth=", "=", authValue);
-    sslConfigFilePath = argUtil.getValue("-c", null);
-    sslConfigFilePath = argUtil.getValueFromArg("--ssl-config=", "=", sslConfigFilePath);
-
+    sslValue = argUtil.getValue("-s", null);
+    sslValue = argUtil.getValueFromArg("--ssl=", "=", sslValue);
+    proxyValue = argUtil.getValue("-P", null);
+    proxyValue = argUtil.getValueFromArg("--proxy=", "=", proxyValue);
+    proxySslValue = argUtil.getValue("-S", null);
+    proxySslValue = argUtil.getValueFromArg("--proxy-ssl=", "=", proxySslValue);
 
     SocksServerBuilder builder = null;
     if (authValue == null) {
@@ -84,11 +95,29 @@ public class Socks5Server {
 
     }
     builder.setBindPort(port);
-    if (sslConfigFilePath != null) {
-      SSLConfiguration configuration = SSLConfiguration.load(sslConfigFilePath);
+    if (sslValue != null) {
+      SSLConfiguration configuration = SSLConfiguration.load(sslValue);
       builder.useSSL(configuration);
     }
-
+    if (proxyValue != null) {
+      String[] values = proxyValue.split(":");
+      if (values.length < 2 || values.length > 4) {
+        logger.error("--proxy value error");
+      }
+      String proxyHost = values[0];
+      int proxyPort = Integer.parseInt(values[1]);
+      if (proxySslValue != null) {
+        proxy =
+            new SSLSocks5(new InetSocketAddress(proxyHost, proxyPort), SSLConfiguration.load
+                (proxySslValue));
+      } else {
+        proxy = new Socks5(proxyHost, proxyPort);
+      }
+      if (values.length == 4) {
+        proxy.setCredentials(new UsernamePasswordCredentials(values[2], values[3]));
+      }
+    }
+    builder.setProxy(proxy);
     server = builder.build();
 
     final SocksProxyServer finalServer = server;
@@ -107,12 +136,17 @@ public class Socks5Server {
    */
   public void showHelp() {
     System.out.println("Usage: [Options]");
-    System.out.println("    -p, --port=<val>             Server bind port");
-    System.out.println("    -a, --auth=<val1:val2>       Use username/password authentication");
-    System.out.println("                                 Example: --auth=admin:1234");
-    System.out.println("                                          --auth=admin:1234,root:1234");
-    System.out.println("    -c, --ssl-config=<val>       SSL configuration file");
-    System.out.println("    -h, --help                   Show help");
+    System.out.println("    -p, --port=<val>         Server bind port");
+    System.out.println("    -a, --auth=<val1:val2>   Use username/password authentication");
+    System.out.println("                             Example: --auth=admin:1234");
+    System.out.println("                                      --auth=admin:1234,root:1234");
+    System.out.println("    -s, --ssl=<val>          SSL configuration file path");
+    System.out.println("    -P, --proxy=<val>        Set server SOCKS5 proxy, <val> should be:");
+    System.out.println("                                 host:port");
+    System.out.println("                             Use Username/Password authentication:");
+    System.out.println("                                 host:port:username:password");
+    System.out.println("    -S, --proxy-ssl=<val>    Proxy SSL configuration file path");
+    System.out.println("    -h, --help               Show help");
   }
 
   /**
